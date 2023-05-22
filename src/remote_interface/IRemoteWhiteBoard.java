f0509176c559a1751c232a22fb5c2ec9283935a6
Chaoyi-Shi <stevenshichaoyi@gmail.com>
2023-05-22 12:18:29 +1000

    update the Rmoteinterface of whiteboard

--- src/remote_interface/IRemoteWhiteBoard.java
+++ src/remote_interface/IRemoteWhiteBoard.java
@@ -7,10 +7,11 @@ import java.rmi.RemoteException;
 public interface IRemoteWhiteBoard extends Remote {
 
     public String getName() throws RemoteException;
-    public String getMode() throws RemoteException;
     public String getText() throws RemoteException;
-    public String getState() throws RemoteException;
+    public String getMode() throws RemoteException;
     public Color getColor() throws RemoteException;
     public Point getPoint() throws RemoteException;
+    public String getState() throws RemoteException;
+
 
 }
